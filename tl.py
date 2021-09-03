3abeda4cab203c156a3750c7e1d2faea992262ad
profgabriel88 <59454245+profgabriel88@users.noreply.github.com>
2021-09-03 08:51:18 -0400

    Update tl.py

--- tl.py
+++ tl.py
@@ -1,40 +1,61 @@
-import urllib.request
+'''
+Tira fotos em intervalos constantes para elaboração de time lapse
+Gabriel Dai
+'''
+
 import numpy as np
 import cv2 as cv
 from os import system
 from time import sleep
+import sys
 
-# numeração do arquivo
 i = 0
+p = 0
+
+# cria pasta para armazenar as fotos
+system(f'mkdir {p}')
 
 # intervalo em segundo entre as fotos
 intervalo = 0.01
 
-# endereço ip para usar o celular como camera - App IP Webcam por Pavel Khlebovich
+# linkpara usar o celular como webcam através de aplicativo.
 url = "http://:8080/video"
 
 
 while True:
-	# se for usar o celular colocar entre parenteses url
-	# cap = cv.VideoCapture(url)
-	# se for usar webcam troque o url por um número, 
-	# 0 para a camera do notebook ou 1 para webcam usb
-    cap = cv.VideoCapture(0)
-    
-    ret, frame = cap.read()
-    cv.imwrite('frame{0:04d}.jpg'.format(i), frame)
-    cv.imshow('frame', frame)
-    cap.release()       
-    print(f'foto {i} capturada')
-    #sleep(0.2)
-           
-    sleep(intervalo)
-    
-    i += 1
-    
-    #apertando q o programa encerra
-    if cv.waitKey(1) == ord('q'):
-       break
+    try:
+	
+        cap = cv.VideoCapture(url)
+        ret, frame = cap.read()
+        cv.imwrite(f'{p}/frame{i:04d}.jpg', frame)
+        
+	# para exibir a imagem capturada remova o comentário da linha a seguir
+	#cv.imshow('frame', frame)
+	
+        cap.release()  
+	
+	# contador de imagens
+        print(f'foto {i} capturada')
+        
+        sleep(0.2)
+            
+        sleep(intervalo)
+        
+        i += 1
+        
+	# a cada 500 fotos uma nova pasta é criada
+        if i == 499:
+            i = 0
+            p += 1
+            system(f'mkdir {p}')
+
+    # caso a conexão com o celular seja interrompida ou perdida
+    # o programa imprime o erro e tenta executar novamente até
+    # restabelecer a conexão
+    except Exception as e:
+        # e = sys.exc_info()[0]
+        if e.code == -215:
+            print( "<p>Error: %s</p>" % e )
 
 print('done')
 
